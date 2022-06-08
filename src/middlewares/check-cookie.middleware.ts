a8b794bbc0bf2c2b364c2a6e892fcb0acc4e8b16
lizheng <v-lizheng@expediagroup.com>
2022-06-08 11:32:11 +0800

    add cookie

---
+++ src/middlewares/check-cookie.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction,Response,Request } from "express";
+import StatusCodes from "http-status-codes";
+import HttpException from "../exception/HttpException";
+import Admin from "../models/Admin";
+
+export const checkAdminCookieMiddleware=async(req:Request,_res:Response,next:NextFunction)=>{
+    const userId=req.cookies['userId'];
+    if (userId) {
+        try {
+            const admin = await Admin.findById(userId);
+            if (admin) {
+                //这里需要存储管理员并将管理员信息存储在req中
+                req.currentAdmin=admin
+                return next()
+               } else {
+                   return next(new HttpException(StatusCodes.UNAUTHORIZED, 'Not such admin'));
+               }
+            
+        } catch (error) {
+            return next(new HttpException(StatusCodes.UNAUTHORIZED, 'Invalid/Expired cookie'));
+        }
+             
+    }
+    next(new HttpException(StatusCodes.UNAUTHORIZED, 'cookie must be provided'));
+    
+}
